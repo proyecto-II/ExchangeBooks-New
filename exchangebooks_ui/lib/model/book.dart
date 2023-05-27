397a94262feefffbacb5c16ff711daa6bc996fba
NICOLÁS HIDALGO <n.hidalgo02@ufromail.cl>
2023-05-27 14:57:25 -0400

    Cambios en el modelo Book

--- exchangebooks_ui/lib/model/book.dart
+++ exchangebooks_ui/lib/model/book.dart
@@ -1,16 +1,30 @@
+import 'package:exchangebooks_ui/model/genre.dart';
+
 class Book {
   String? id;
   String? title;
   String? author;
   String? description;
+  List<Genre>? genres;
+  String? type;
+  String? image;
 
-  Book(this.id, this.title, this.author, this.description);
+  Book(this.id, this.title, this.author, this.description, this.genres,
+      this.type, this.image);
 
   Book.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     title = json['title'];
     author = json['author'];
     description = json['description'];
+    if (json['genres'] != null) {
+      genres = <Genre>[];
+      json['genres'].forEach((v) {
+        genres!.add(Genre.fromJson(v));
+      });
+    }
+    type = json['type'];
+    image = json['image'];
   }
 
   Map<String, dynamic> toJson() {
@@ -19,6 +33,11 @@ class Book {
     data['title'] = title;
     data['author'] = author;
     data['description'] = description;
+    if (genres != null) {
+      data['genres'] = genres!.map((v) => v.toJson()).toList();
+    }
+    data['type'] = type;
+    data['image'] = image;
     return data;
   }
 }
