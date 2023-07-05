5fd3623f6712e43b51fbcb4d294abf3af7fe20c5
NICOLAS ALEJANDRO PEREIRA VEGA <n.pereira01@ufromail.cl>
2023-07-05 12:14:30 -0400

    configurado book service

--- exchangebooks_book/src/services/book.service.js
+++ exchangebooks_book/src/services/book.service.js
@@ -1,4 +1,4 @@
-import { AUTH_SERVICE_URL } from "../config/constants.js";
+import { AUTH_SERVICE_URL, GENRE_SERVICE_URL } from "../config/constants.js";
 import Book from "../models/Book.js";
 import axios from "axios";
 
@@ -6,16 +6,16 @@ class BookService {
   constructor() {}
 
   /**
-  * Metodo que obtiene todos los libros de la base de datos, igualmente realiza una busqueda de los usuarios para dar una información más detallada de las publicaciones
-  * @return lista de los libros guardados en la base de datos
-  */
+   * Metodo que obtiene todos los libros de la base de datos, igualmente realiza una busqueda de los usuarios para dar una información más detallada de las publicaciones
+   * @return lista de los libros guardados en la base de datos
+   */
   async getAll() {
     const books = await Book.find().exec();
     const fetchCategories = books.map(async (book) => {
       try {
         const { genres, userId, ...others } = book._doc;
         const { data, status: genreStatus } = await axios.post(
-          "http://localhost:3002/list",
+          `${GENRE_SERVICE_URL}/list`,
           {
             genres,
           }
@@ -37,10 +37,10 @@ class BookService {
   }
 
   /**
-  * Metodo que obtiene un libro en especifico segun su id
-  * @param id Es la id del libro
-  * @return el libro en especifico
-  */
+   * Metodo que obtiene un libro en especifico segun su id
+   * @param id Es la id del libro
+   * @return el libro en especifico
+   */
   async getById(id) {
     const book = await Book.findById(id);
     try {
@@ -66,40 +66,39 @@ class BookService {
   }
 
   /**
-  * Metodo que permite guardar un libro en la base de datos
-  * @param book Es el libro con todo sus atributos
-  * @return el libro guardado en la base de datos
-  */
+   * Metodo que permite guardar un libro en la base de datos
+   * @param book Es el libro con todo sus atributos
+   * @return el libro guardado en la base de datos
+   */
   async create(book) {
     const newBook = new Book(book);
     return await newBook.save();
   }
 
   /**
-  * Metodo que permite editar un libro en especifico
-  * @param id Es el id del libro
-  * @param book Es el libro con sus atributos editados
-  * @return el libro guardado en la base de datos ya editado
-  */
+   * Metodo que permite editar un libro en especifico
+   * @param id Es el id del libro
+   * @param book Es el libro con sus atributos editados
+   * @return el libro guardado en la base de datos ya editado
+   */
   async edit(id, book) {
     return await Book.findByIdAndUpdate(id, book, { new: true });
   }
 
   /**
-  * Metodo que permite eliminar un libro
-  * @param id Es el id del libro
-  * @return un mensaje de eliminación exitosa
-  */
+   * Metodo que permite eliminar un libro
+   * @param id Es el id del libro
+   * @return un mensaje de eliminación exitosa
+   */
   async delete(id) {
     return await Book.findByIdAndDelete(id);
   }
 
-
   /**
-  * Metodo que busca todos los libros de un usuario
-  * @param userId Es el id del usuario
-  * @return la lista de todos los libros que el usuario ha publicado
-  */
+   * Metodo que busca todos los libros de un usuario
+   * @param userId Es el id del usuario
+   * @return la lista de todos los libros que el usuario ha publicado
+   */
   async getBooksByUser(userId) {
     const books = await Book.find({ userId: userId }).exec();
     const fetchCategories = books.map(async (book) => {
@@ -119,10 +118,10 @@ class BookService {
   }
 
   /**
-  * Metodo que busca los libros segun los parametros indicados por el usuario en el frontend
-  * @param query es el parametro que permite realizar la busqueda de los libros(puede ser el titulo o el autor)
-  * @return la lista de libros filtrada segun los parametros indicados
-  */
+   * Metodo que busca los libros segun los parametros indicados por el usuario en el frontend
+   * @param query es el parametro que permite realizar la busqueda de los libros(puede ser el titulo o el autor)
+   * @return la lista de libros filtrada segun los parametros indicados
+   */
   async search(query) {
     const books = await Book.find({
       $or: [
